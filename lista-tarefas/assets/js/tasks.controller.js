1597a515f6447c41e581813453174980eee1ebd3
Matheus <matheusalbuquerqq08@gmail.com>
2025-05-02 10:57:06 -0300

    Tarefas

--- lista-tarefas/assets/js/tasks.controller.js
+++ lista-tarefas/assets/js/tasks.controller.js
@@ -3,75 +3,84 @@ app.controller("TaskController", function ($scope, $filter, TaskService) {
     $scope.showCompletedOnly = false;
     $scope.showIncompletedOnly = false;
     $scope.showTodayOnly = false;
-    $scope.today = new Date().toLocaleDateString();
-    $scope.tasks = TaskService.getTasks();
+    $scope.today = new Date().toLocaleDateString()
+    $scope.tasks = TaskService.getTasks()
     $scope.taskInput = {
         title: "",
         date: "",
-    };
+    }
 
     $scope.filteredTasks = function () {
-        let filtered = $filter('filter')(
-            $filter('filter')($scope.tasks,
+        let filtered = $filter("filter")(
+            $filter("filter")(
+                $scope.tasks,
                 $scope.showCompletedOnly ? { checked: true } : {}
-
             ),
             $scope.showIncompletedOnly ? { checked: false } : {}
-        );
+        )
 
         if ($scope.showTodayOnly) {
             const start = new Date()
-            start.setHours(0);
-            start.setMinutes(0);
-            start.setSeconds(0);
-            start.setMilliseconds(0);
+            start.setHours(0)
+            start.setMinutes(0)
+            start.setSeconds(0)
+            start.setMilliseconds(0)
             const end = new Date()
-            end.setHours(23);
-            end.setMinutes(59);
-            end.setSeconds(59);
-            end.setMilliseconds(999);
+            end.setHours(23)
+            end.setMinutes(59)
+            end.setSeconds(59)
+            end.setMilliseconds(999)
+
             filtered = filtered.filter((task) => {
                 const date = new Date(task.date)
                 return (
                     date.getTime() >= start.getTime() && date.getTime() <= end.getTime()
-                );
-            });
-
+                )
+            })
         }
 
-        return filtered;
-    };
+        return filtered
+    }
 
-    $scope.togglemodal = () => {
-        $scope.modalActive = !$scope.modalActive;
-    };
+    $scope.toggleModal = () => {
+        $scope.modalActive = !$scope.modalActive
+    }
 
     $scope.handleSubmitAddTask = () => {
         const title = $scope.taskInput.title
         const date = $scope.taskInput.date
-        if (!title || !date) return;
-
-        TaskService.addTask(title, date);
-        $scope.tasks = TaskService.getTasks();
+        if (!title || !date) return
 
 
+        TaskService.addTask(title, date)
+        $scope.tasks = TaskService.getTasks()
 
-
-
-        $scope.togglemodal();
+        $scope.toggleModal()
         $scope.taskInput.title = "";
         $scope.taskInput.date = "";
-    };
+    }
 
     $scope.toggleCheckedTask = () => {
-        TaskService.toggleCheck();
-        $scope.tasks = TaskService.getTasks();
-
-    };
+        TaskService.toggleCheck()
+        $scope.tasks = TaskService.getTasks()
+    }
 
     $scope.deleteTask = (currentTask) => {
-        TaskService.removeTask(currentTask.id);
-        $scope.tasks = TaskService.getTasks();
+        TaskService.removeTask(currentTask.id)
+        $scope.tasks = TaskService.getTasks()
+    }
 
-    };
-});
+    $scope.validate = (error, touched) => {
+        console.log(touched)
+        if (!touched)
+            return {}
+        const values = Object.values(error)
+        if (values.length === 0)
+            return {}
+
+        const isTrue = values.reduce((acc, curr) => acc && curr, true)
+        if (isTrue)
+            return { "border-color": "red" }
+    }
+
+})
